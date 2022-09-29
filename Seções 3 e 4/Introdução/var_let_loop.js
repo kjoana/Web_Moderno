c740b556c0732e0a24e93d46abf93da0a57cbfe6
kjoana <114084176+kjoana@users.noreply.github.com>
2022-09-29 10:49:08 -0300

    Create var_let_loop.js

---
+++ Seções 3 e 4/Introdução/var_let_loop.js
@@ -0,0 +1,13 @@
+
+// usando var em loop
+for (var i =0; i<10;i++){
+    console.log(i)
+}
+console.log(i)
+
+// usando let em loop
+
+for (let i =0; i<10;i++){
+    console.log(i)
+}
+console.log(i) // i não vai estar definido, pois ela só existe dentro do bloco
