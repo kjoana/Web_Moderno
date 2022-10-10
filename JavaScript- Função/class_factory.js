6072cd61578b0ce4e9eae9ddf81f275e33142018
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-10 09:24:43 -0300

    Create class_factory.js

---
+++ JavaScript- Função/class_factory.js
@@ -0,0 +1,27 @@
+// class
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+// factory
+
+/*function Pessoa(nome) {
+    this.nome = nome
+    
+    this.falar = function() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+console.log(p1.nome)*/
