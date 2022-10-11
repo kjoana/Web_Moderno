950c7b04d5af9014c95e27a3d4d73ae39370ac6d
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-11 10:41:54 -0300

    Create quest10.js

---
+++ Exercícios_CursoWebModerno/quest10.js
@@ -0,0 +1,9 @@
+function divisivelPor3(numero){
+    if (numero%3 == 0 ){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(divisivelPor3(30))
+console.log(divisivelPor3(34))
