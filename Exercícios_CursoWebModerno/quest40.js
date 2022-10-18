c1a83906ccfde14ff5b24b76816a49c3a2a5c3cc
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-18 09:50:06 -0300

    Create quest40.js

---
+++ Exercícios_CursoWebModerno/quest40.js
@@ -0,0 +1,20 @@
+function conceitoNotas(notas) {
+    let conceitos = []
+    for(let i = 0; i < notas.length; i++) {
+        if(notas[i]>=0 && notas[i]<=4.9){
+            conceitos.push('D')
+        } else if(notas[i]>=5 && notas[i]<=6.9) {
+            conceitos.push('C')            
+        } else if(notas[i]>=7 && notas[i]<=8.9) {
+            conceitos.push('B')
+        } else if(notas[i]>=9 && notas[i]<=10){
+            conceitos.push('A')
+        } else {
+            conceitos.push('Nota inválida')       
+       }
+    }
+    return conceitos
+}
+
+notas = [ 1,7,10,5.5]
+console.log(conceitoNotas(notas))
