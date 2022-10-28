815856f44da56074643b8ea1ac5d57221dbeca59
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-28 12:26:27 -0300

    Create quest5.js

---
+++ Exercícios_2_CursoWebModerno/quest5.js
@@ -0,0 +1,10 @@
+function igualMaior(valor1, valor2){
+    if (valor1>valor2)
+        return true
+    else if (valor1==valor2)
+        return true
+    else
+        return false
+}
+
+console.log(igualMaior(7,7))
