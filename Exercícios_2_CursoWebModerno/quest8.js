43721c413c86f70749c79ff2b9ec80b71023cd42
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-28 12:26:41 -0300

    Create quest8.js

---
+++ Exercícios_2_CursoWebModerno/quest8.js
@@ -0,0 +1,7 @@
+function multiplicar(numeroA, numeroB) {
+    let resultado = 0
+    for(let i = 0; i < numeroB; i++)
+    resultado += numeroA
+    return resultado
+    }
+console.log(multiplicar(5,2))
