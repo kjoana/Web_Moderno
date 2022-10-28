c1309686d23db66eceb49b3909f9351fe4bdf5a3
kjoana <114084176+kjoana@users.noreply.github.com>
2022-10-28 12:26:21 -0300

    Create quest4.js

---
+++ Exercícios_2_CursoWebModerno/quest4.js
@@ -0,0 +1,7 @@
+function receberNomeDoMes(numero) {
+    const mapeamento = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+    'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    return mapeamento[--numero];
+    }
+
+console.log(receberNomeDoMes(4))
