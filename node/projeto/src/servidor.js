79adeb3f6f9aae5fa6d7ef47ba365fd2fba44a43
kjoana <114084176+kjoana@users.noreply.github.com>
2022-11-18 12:09:40 -0300

    Update servidor.js

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,10 +2,24 @@ const porta =  3003
 
 const express = require('express')
 const app = express()
+const bancodedados = require('.../bancodedados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome:"Notebook", preco: 123.45})// converter para JSON
+    res.send(bancoDeDados.getProdutos())
 })
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
